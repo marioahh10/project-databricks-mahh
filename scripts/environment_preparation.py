f1d3ae8b6abd67d62bf3289863b48d8925e54715
marioahh10 <mariohh.96@hotmail.com>
2025-12-10 20:56:08 -0500

    Create environment_preparation.py

---
+++ scripts/environment_preparation.py
@@ -0,0 +1,11 @@
+def create_schemas(spark):
+    """
+    Crea los schemas necesarios para la arquitectura Medallion.
+    """
+    spark.sql("CREATE SCHEMA IF NOT EXISTS retail_bronze")
+    spark.sql("CREATE SCHEMA IF NOT EXISTS retail_silver")
+    spark.sql("CREATE SCHEMA IF NOT EXISTS retail_gold")
+
+
+if __name__ == "__main__":
+    create_schemas(spark)
